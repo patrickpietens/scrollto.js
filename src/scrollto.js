0435a5796feb2add4a2009fe97b0dc8bab6b664d
Patrick Pietens <hello@patrickpietens.com>
2016-08-17 23:34:59 +0200

    Refactor code

--- src/scrollto.js
+++ src/scrollto.js
@@ -2,99 +2,89 @@
 
 import raf from 'raf';
 
-// Easing function used to scroll DOMNodes and the window
+// Helper function which is used to ease the scrolling
 const easeOutCubic = (t, b, c, d) => c * ((t = t / d - 1) * t * t + 1) + b;
 
-// Default options used to scroll DOMNodes and the window
+// Helper function to clamp a value between a min and max value
+const clamp = (value, min, max) => Math.min(Math.max(value, min), max);
+
+// Default options used when a new scroll is initiated
 const defaultOptions = {
     horizontal: true,
     vertical: true,
     duration: 500,
+    easing: easeOutCubic,
     offsetX: 0,
     offsetY: 0,
 };
 
-/**
- * Convenience method to set the scrollposition of an element
- * @param  {number} x Horizontal position
- * @param  {number} y Vertical position
- */
-Node.prototype.scrollTo = function (x, y) {
-    this.scrollLeft = x;
-    this.scrollTop = y;
-};
+export function scrollToPosition(target, x, y, options = {}) {
+    return new Promise((resolve, reject) => {
+        options = Object.assign({}, defaultOptions, options);
 
-/**
- * Smooth scroll an element or window object to a specific position
- * @param  {number} x       Horizontal position
- * @param  {number} y       Vertical position
- * @param  {object} options Object containing options used to scroll the element
- */
-Node.prototype.scrollToPosition = window.scrollToPosition = function (x, y, options = {}) {
-    options = Object.assign({}, defaultOptions, options);
+        // Cancel previous tweens on the DOM element
+        if (!!target._scrollRequest) {
+            raf.cancel(target._scrollRequest);
+    	}
 
-    // Cancel previous tweens on the DOM element
-    if (!!this._scrollRequest) {
-        raf.cancel(this._scrollRequest);
-	}
-
-    // Set the scroll properties directly on the element when the duration is 0 or less
-    if (options.duration <= 0) {
-        return this.scrollTo(x, y);
-    }
-
-    // Set initial constants needed for easing
-    const initialX = this === window ? window.pageXOffset : this.scrollLeft;
-    const initialY = this === window ? window.pageYOffset : this.scrollTop;
-
-    const distanceX = x - initialX;
-    const distanceY = y - initialY;
-
-    let myStartTime;
-
-    raf(function tick(time) {
-        // Set the time the animation started
-        if (!myStartTime) {
-            myStartTime = time;
+        // Set the scroll properties directly on the element when the duration is 0 or less
+        if (options.duration <= 0) {
+            target.scrollTo(x, y);
+            return resolve(target);
         }
 
-        // Calculate the elapsed time
-        let myElapsedTime = time - myStartTime;
+        // Get the initial position of the target element
+        const initialX = (target === window) ? window.pageXOffset : target.scrollLeft;
+        const initialY = (target === window) ? window.pageYOffset : target.scrollTop;
 
-        // Set the target scroll values when the animation is finished
-        if (myElapsedTime >= options.duration) {
-            this.scrollTo(x, y);
+        // Calculate the max scroll positions
+        const limitX = target.scrollWidth - target.innerWidth;
+        const limitY = target.scrollHeight - target.innerHeight;
 
-            // Cleanup temporary variables
-            raf.cancel(this._scrollRequest);
-            this._scrollRequest = null;
+        // Calculate the
+        const distanceX = clamp(x, 0, limitX) - initialX;
+        const distanceY = clamp(y, 0, limitY) - initialY;
 
-            return;
-        }
+        let myStartTime;
 
-        // Otherwise calculate the scroll values using the custom easing function
-        let myX = easeOutCubic(myElapsedTime, initialX, distanceX, options.duration),
-            myY = easeOutCubic(myElapsedTime, initialY, distanceY, options.duration);
+        target._scrollRequest = raf(function tick(time) {
+            // Set the time the animation started
+            if (!myStartTime) {
+                myStartTime = time;
+            }
 
-        this.scrollTo(myX, myY);
-        this._scrollRequest = raf(tick.bind(this));
+            // Calculate the elapsed time
+            let myElapsedTime = time - myStartTime;
 
-    }.bind(this));
-};
+            // Set the target scroll values when the animation is finished
+            if (myElapsedTime >= options.duration) {
+                target.scrollTo(x, y);
 
-/**
- * Scroll an element or the window to a target element
- * @param  {Node} element Target element to scroll to
- * @param  {object} options Object containing options used to scroll the element
- */
-Node.prototype.scrollToElement = window.scrollToElement = function (element, options = {}) {
+                // Cleanup temporary variables
+                raf.cancel(target._scrollRequest);
+                target._scrollRequest = null;
+
+                return resolve(target);
+            }
+
+            // Otherwise calculate the scroll values using the custom easing function
+            let myX = options.easing(myElapsedTime, initialX, distanceX, options.duration),
+                myY = options.easing(myElapsedTime, initialY, distanceY, options.duration);
+
+            target.scrollTo(myX, myY);
+            target._scrollRequest = raf(tick);
+        });
+    });
+}
+
+export function scrollToElement(target, element, options = {}) {
     options = Object.assign({}, defaultOptions, options);
 
     // Get the bounding box from the target element
     const boundingBox = element.getBoundingClientRect();
 
-    let myX = (this === window ? window.pageXOffset : this.scrollLeft),
-        myY = (this === window ? window.pageYOffset : this.scrollTop);
+    let myX = (target === window ? window.pageXOffset : this.scrollLeft),
+        myY = (target === window ? window.pageYOffset : this.scrollTop);
 
     // Set horizontal position if this is allowed
     if (options.horizontal) {
@@ -111,5 +101,66 @@ Node.prototype.scrollToElement = window.scrollToElement = function (element, opt
     myY += options.offsetY;
 
     // Scroll to the position of the element
-    this.scrollToPosition(myX, myY, options);
-};
+    return scrollToPosition(target, myX, myY, options);
+}
+
+if (!Node.prototype.scrollTo) {
+    /**
+     * Convenience method to set the scroll position of an element. The description is identical to window.scrollTo.
+     * @param  {number} x Horizontal position
+     * @param  {number} y Vertical position
+     */
+    Node.prototype.scrollTo = function (x, y) {
+        this.scrollLeft = x;
+        this.scrollTop = y;
+    };
+}
+
+if (!('scrollWidth' in window)) {
+    /**
+     * Convencience property to get the scrollWidth of the document. The description is identical to element.scrollWidth.
+     */
+    Object.defineProperty(window, 'scrollWidth', {
+        get: () => Math.max(
+            document.body.clientWidth,
+            document.documentElement.clientWidth,
+            document.documentElement.scrollWidth
+        ),
+    });
+}
+
+if (!('scrollHeight' in window)) {
+    /**
+     * Convencience property to get the scrollWidth of the document. The description is identical to element.scrollWidth.
+     */
+    Object.defineProperty(window, 'scrollHeight', {
+        get: () => Math.max(
+            document.body.clientHeight,
+            document.documentElement.clientHeight,
+            document.documentElement.scrollHeight
+        ),
+    });
+}
+
+if (!Node.prototype.scrollToPosition) {
+    /**
+     * Smooth scroll an element or window object to a specific position
+     * @param  {number} x       Horizontal position
+     * @param  {number} y       Vertical position
+     * @param  {object} options Object containing options used to scroll the element
+     */
+    Node.prototype.scrollToPosition = window.scrollToPosition = function (x, y, options = {}) {
+        return scrollToPosition(this, x, y, options);
+    };
+}
+
+if (!Node.prototype.scrollToElement) {
+    /**
+     * Scroll an element or the window to a target element
+     * @param  {Node} element Target element to scroll to
+     * @param  {object} options Object containing options used to scroll the element
+     */
+    Node.prototype.scrollToElement = window.scrollToElement = function (element, options = {}) {
+        return scrollToElement(this, element, options);
+    };
+}
